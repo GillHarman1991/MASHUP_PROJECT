e42772534c145465cf3ea8caf9b54e508f42a87a
gillharman1991 <742456@student.tor.loc>
2019-04-11 10:46:46 -0400

    finaleditingto index.html

--- mapscript.js
+++ mapscript.js
@@ -13,14 +13,15 @@ function initialize(){
             {
                 lat:40.758904,lng:-73.985128
             }};
-            var mapOptions2={
+            var mapOptions3={
                 zoom:18,
                 center:
                 {
                     lat:40.758904,lng:-73.985128
                 }};
-            map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOprions1);
-            map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOprions2);
-            map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOprions3);
+            map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOptions1);
+            map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOptions2);
+            map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOptions3);
             
-}google.maps.event.addDomListener(window,'load',initialize);
+}
+google.maps.event.addDomListener(window,'load',initialize);
