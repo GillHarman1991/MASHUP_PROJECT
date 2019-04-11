c4287070ddf055863a29789c8858d66e5ce12b9b
gillharman1991 <742456@student.tor.loc>
2019-04-11 10:35:06 -0400

    fewchanges

--- mapscript.js
+++ mapscript.js
@@ -14,13 +14,13 @@ function initialize(){
                 lat:40.758904,lng:-73.985128
             }};
             var mapOptions2={
-                zoom:10,
+                zoom:18,
                 center:
                 {
                     lat:40.758904,lng:-73.985128
                 }};
-            map1=new google.maps.Map(document.getElementById('mao_canvas1'),mapOprions1);
-            map2=new google.maps.Map(document.getElementById('mao_canvas2'),mapOprions2);
-            map3=new google.maps.Map(document.getElementById('mao_canvas3'),mapOprions3);
+            map1=new google.maps.Map(document.getElementById('map_canvas1'),mapOprions1);
+            map2=new google.maps.Map(document.getElementById('map_canvas2'),mapOprions2);
+            map3=new google.maps.Map(document.getElementById('map_canvas3'),mapOprions3);
             
 }google.maps.event.addDomListener(window,'load',initialize);
